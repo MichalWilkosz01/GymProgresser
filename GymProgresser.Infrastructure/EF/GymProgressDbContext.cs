885763bc9438f5ca01ab02fd770c643ce0a20538
Michal <mw7883475@gmail.com>
2025-04-08 23:33:44 +0200

    First migration

---
+++ GymProgresser.Infrastructure/EF/GymProgressDbContext.cs
@@ -0,0 +1,26 @@
+﻿using GymProgresser.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymProgresser.Infrastructure.EF
+{
+    public class GymProgressDbContext : DbContext
+    {
+        public GymProgressDbContext(DbContextOptions<GymProgressDbContext> options) : base(options) { }
+
+
+        public DbSet<BodyMeasurement> BodyMeasurements { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<ExerciseWorkout> ExercisesWorkouts { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Workout> Workouts { get; set; }
+
+        
+    }
+}
